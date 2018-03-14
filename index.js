45810eb13934ab3c5d03b98ff0e60e0f5191d990
Atticus White <contact@atticuswhite.com>
2018-03-13 22:21:14 -0400

    HoC

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
-class ViewTransition extends React.Component {
-  state = {
-    transitions: {}
+const Transition = {
+  View: withStyleTransitions(Animated.View),
+  Text: withStyleTransitions(Animated.Text)
+}
+
+function withStyleTransitions (WrappedComponent) {
+  class Transition extends React.Component {
+    state = {
+      transitions: {}
+    }
+
+    constructor(props) {
+      super(props)
+
+      this.state.transitions = Object.keys(this.props.style).reduce((transitions, key) => {
+        transitions[key] = new Animated.Value(this.props.style[key])
+        return transitions
+      }, {});
+    }
+
+    componentWillReceiveProps(nextProps) {
+      let styleDiffs = diff(nextProps.style, this.props.style)
+
+      styleDiffs.forEach(key => {
+        Animated.timing(
+          this.state.transition[key]
+          {
+            toValue: nextProps.style[key],
+            duration: this.props.duration
+          }
+        ).start()
+      });
+    }
+
+    render() {
+      return (
+        <WrappedComponent style={[this.props.style, this.state.transitions]}>
+          {this.props.children}
+        </WrappedComponent>
+      )
+    }
   }
 
-  constructor(props) {
-    super(props)
-
-    this.state.transitions = Object.keys(this.props.style).reduce((transitions, key) => {
-      transitions[key] = new Animated.Value(this.props.style[key])
-      return transitions
-    }, {});
-  }
-
-  componentWillReceiveProps(nextProps) {
-    let styleDiffs = diff(nextProps.style, this.props.style)
-
-    styleDiffs.forEach(key => {
-      Animated.timing(
-        this.state.transition[key]
-        {
-          toValue: nextProps.style[key],
-          duration: this.props.duration
-        }
-      ).start()
-    });
-  }
-
-  render() {
-    return (
-      <Animated.View style={[this.props.style, this.state.transitions]}>
-        {this.props.children}
-      </Animated.View>
-    )
-  }
+  return Transition
 }
