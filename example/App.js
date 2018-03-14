a11b21a64471a8c13e86764e0ca80f5060a5106a
Atticus White <contact@atticuswhite.com>
2018-03-14 06:24:19 -0400

    working example

--- example/App.js
+++ example/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Transition from './transition';
 
 export default class App extends React.Component {
+  state = {
+    visible: true
+  }
   render() {
     return (
       <View style={styles.container}>
+        <TouchableOpacity onPress={() => this.setState({visible: !this.state.visible})}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        </TouchableOpacity>
+
+        <Transition.Text
+          style={{opacity: this.state.visible ? 1 : 0}}
+          duration={1000}
+        >
+          Hello World
+        </Transition.Text>
+
+
       </View>
     );
   }
